ee837f9672d71814be6e143aad3db4c4dc2b44d9
Michael Kolle <michael.kolle@wago.com>
2025-03-20 14:23:16 +0100

    conntect to container and find main.py

--- src/debugger/find_main_auf_controller.sh
+++ src/debugger/find_main_auf_controller.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+docker exec -it 2c9b /bin/sh
+
 # Suchen nach der main.py Datei im src Verzeichnis
-MAIN_PY_PATH=$(find /pfad/zum/controller "main.py")
+MAIN_PY_PATH=$(find ./main.py "main.py")
 
 # Debugpy starten und auf Verbindung warten 
 python -m debugpy --listen 0.0.0.0:5678 --wait-for-client "$MAIN_PY_PATH"
