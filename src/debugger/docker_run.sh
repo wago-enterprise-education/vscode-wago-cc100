3973cb817ba6a3985fc66bce93f9b0a953fd4229
Michael Kolle <michael.kolle@wago.com>
2025-03-21 08:30:41 +0100

    docker run command and new main.py with debugpy

---
+++ src/debugger/docker_run.sh
@@ -0,0 +1,8 @@
+#Docker run befehl zum Mappen der Ports und dem Bindmounts
+docker run --rm -d \
+-v '/sys/kernel/dout_drv/DOUT_DATA:/sys/kernel/dout_drv/DOUT_DATA' \
+-p 8765:5678 \
+05fd
+
+#Port öffnen
+Ich muss keinen Port öffnen, da keine Firewall
