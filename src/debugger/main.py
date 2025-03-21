b7ae25e8f1f4fd57d0f8349b2f5c9800581496ba
Michael KOLLE <Michael.Kolle@wago.com>
2025-03-21 08:51:31 +0100

    updated main.py with debugpy

--- src/debugger/main.py
+++ src/debugger/main.py
@@ -1,7 +1,14 @@
-print("Hello World")
-
-i =1
-while i <=20:
-    print(i)
-    i =i +1
-print("Fertig")
+from lib import CC100IO
+import debugpy
+ 
+ 
+debugpy.listen(("0.0.0.0",5678))
+debugpy.wait_for_client
+ 
+# Sample script for writing digital Outputs
+state = True
+while True:
+    for output in range(1,5):
+        CC100IO.digitalWrite(output, state)
+        CC100IO.delay(100)
+    state = not state
