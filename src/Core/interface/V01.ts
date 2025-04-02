b53c6dada06959e1409ee66d2de307a4e3f3c257
Finn Erik Hilgers <finnerik.hilgers@wago.com>
2025-04-02 15:50:44 +0200

    Enhance EditSettings functionality with async support and improved error handling; add JSON write methods for settings management

--- src/Core/interface/V01.ts
+++ src/Core/interface/V01.ts
@@ -47,8 +47,28 @@ export class ConfigureController implements Interface.ConfigureControllerInterfa
     }
 }
 export class EditSettings implements Interface.EditSettingsInterface{
-    editSettings(element: any) {
-        console.log("Edit settings command executed", element);
+    async editSettings(controller: ControllerItem | undefined) {
+        if(!vscode.workspace.workspaceFolders) {
+            vscode.window.showErrorMessage('No workspace is open');
+            return;
+        }
+        let settingToEdit: string;
+        if(controller === undefined) {
+            settingToEdit = await vscode.window.showQuickPick(
+                Object.values(setting),
+                {
+                    title: 'Choose Setting',
+                    canPickMany: false
+                }
+            ) || '';
+            if (!settingToEdit) return;
+
+            await EditSettingsFunctionality.editSetting(0 , settingAdapter[settingToEdit as keyof typeof settingAdapter]);
+        } else {
+            await EditSettingsFunctionality.editSetting(0 , settingAdapter[controller.setting as keyof typeof settingAdapter]);
+        }
+
+        ControllerProvider.instance.refresh();
     }
 }
 export class EstablishConnections implements Interface.EstablishConnectionsInterface{
@@ -84,10 +104,7 @@ export class ViewChildren implements Interface.ViewChildrenInterface{
                 settingArray.push(new ControllerItem(element.controllerId, setting.connection, controller.connection));
                 if(controller.connection === 'ethernet') {
                     settingArray.push(new ControllerItem(element.controllerId, setting.ip, controller.ip));
-                }
-                if(controller.connection === 'simulator') {
-                    settingArray.push(new ControllerItem(element.controllerId, setting.description, controller.simulation_frontend));
-                    settingArray.push(new ControllerItem(element.controllerId, setting.description, controller.simulation_backend));
+                
                 }
                 settingArray.push(new ControllerItem(element.controllerId, setting.port, controller.port));
                 settingArray.push(new ControllerItem(element.controllerId, setting.user, controller.user));
@@ -110,17 +127,21 @@ export class JsonCommands {
         return JSON.parse(fs.readFileSync(`${vscode.workspace.workspaceFolders![0].uri.fsPath}/settings.json`, 'utf8'));
     }
 
+    public static writeJson(id: number, attribute: settingsJson, value: string) {
+        let json = this.getSettingsJson();
+        json[attribute] = value;
+        console.log(JSON.stringify(json));
+        fs.writeFileSync(`${vscode.workspace.workspaceFolders![0].uri.fsPath}/settings.json`, JSON.stringify(json, null, "\t"));
+    }
+
     public static getController() {
         const settings = this.getSettingsJson();
         let connectionType = '';
-        if(settings.usb_c) connectionType = 'usb-c';
-        if(settings.ethernet) connectionType = 'ethernet';
-        if(settings.simulator) connectionType = 'simulator';
+        if(settings. ethernet === 'true') connectionType = 'ethernet';
+        if(settings.usb_c === 'true') connectionType = 'usb-c';
 
         return {
             connection: connectionType,
-            simulation_frontend: settings.simulation_frontend,
-            simulation_backend: settings.simulation_backend,
             ip: settings.ip_adress,
             port: settings.port,
             user: settings.user,
@@ -139,4 +160,138 @@ export enum setting {
     port = 'Port',
     user = 'User',
     autoupdate = 'Autoupdate'
+}
+
+export enum settingAdapter {
+    Name = "displayname",
+    Description = "description",
+    Engine = "engine",
+    Source = "src",
+    'Docker Image Version' = "imageVersion",
+    Connection = "connection",
+    IP = "ip",
+    Port = "port",
+    User = "user",
+    Autoupdate = "autoupdate"
+}
+
+export enum settingsJson {
+    connection = 'connection',
+    usb_c = 'usb_c',
+    simulator = 'simulator',
+    ethernet = 'ethernet',
+    ip_adress = 'ip_adress',
+    port = 'port',
+    user = 'user',
+    autoupdate = 'autoupdate'
+}
+
+export class EditSettingsFunctionality {
+    
+    public static async editSetting(id: number, settingToEdit: string) {
+        if(!vscode.workspace.workspaceFolders) {
+            vscode.window.showErrorMessage('No workspace is open');
+            return;
+        }
+        let content
+        switch (settingToEdit) {
+            // wago.yaml Setting
+            case "description":
+                content = await this.getInput();
+                if (!content) return;
+                // YamlCommands.writeWagoYaml(id, wagoSettings[settingToEdit], content);
+                break;
+
+            // wago.yaml QuickPick
+            case "engine":
+                //TODO - No Enums of available engines yet----------------------------------------------
+                break;
+
+            case "src":
+                const workspacePath = vscode.workspace.workspaceFolders![0].uri.fsPath;
+                const controllerSrc = await vscode.window.showQuickPick(
+                    fs.readdirSync(workspacePath)
+                        .map((folder) => {
+                            if (fs.existsSync(`${workspacePath}/${folder}/main.py`)) {
+                                return {
+                                    label: `${folder}`,
+                                    description: `${folder}/main.py`
+                                };
+                            }
+                            return { label: "" };
+                        })
+                        .filter((path) => path.label.length > 0 ? true : false)
+                        .concat({ label: "New", description: 'Create a new folder' }),
+                    {
+                        title: 'Add Controller',
+                        canPickMany: false
+                    }
+                ) || { label: "" };
+                //if (!controllerSrc) return;
+                
+                //TODO - Actually Create New Folder-----------------------------
+
+                // YamlCommands.writeWagoYaml(id, wagoSettings[settingToEdit], controllerSrc.label);
+                break;
+            case "imageVersion": 
+                //TODO - Not yet determined how they will be managed-------------------------------------------
+                break;
+            case "connection":
+                let conType = await vscode.window.showQuickPick(['usb-c', 'ethernet'], {
+                    title: 'Connection Type',
+                    canPickMany: false
+                }) || '';
+                if (!conType) return;
+                switch (conType) {
+                    case 'usb-c':
+                        JsonCommands.writeJson(0, settingsJson.usb_c, 'true');
+                        JsonCommands.writeJson(0, settingsJson.simulator, 'false');
+                        JsonCommands.writeJson(0, settingsJson.ethernet, 'false');
+                        JsonCommands.writeJson(0, settingsJson.ip_adress, '192.168.42.42');
+                        break;
+                    case 'ethernet':
+                        JsonCommands.writeJson(0, settingsJson.usb_c, 'false');
+                        JsonCommands.writeJson(0, settingsJson.simulator, 'false');
+                        JsonCommands.writeJson(0, settingsJson.ethernet, 'true');
+                        break
+                    default:
+                        break;
+                }
+                break;
+
+            case "ip": 
+                content = await this.getInput();
+                if (!content) return;
+                JsonCommands.writeJson(0, settingsJson.ip_adress, content);
+                break;
+            case "port":
+            case "user":
+                content = await this.getInput();
+                if (!content) return;
+                JsonCommands.writeJson(0, settingsJson[settingToEdit], content);
+                break;
+
+            // controller.yaml QuickPick
+            case "autoupdate": 
+                let status = await vscode.window.showQuickPick(['on', 'off'], {
+                    title: 'Autoupdate',
+                    canPickMany: false
+                }) || '';
+                if (!status) return;
+
+                JsonCommands.writeJson(0, settingsJson[settingToEdit], status);
+                break;
+            default :
+                vscode.window.showErrorMessage("Invalid Attribute Type");
+            break;
+        }
+    }
+
+    private static async getInput(): Promise<string> {
+        let input = await vscode.window.showInputBox({
+            prompt: 'Enter the value the Setting should be set to',
+            title: 'Set Setting Value'
+        }) || '';
+        return input;
+    }
 }
